9d8a5eb4c1844bfa56f53ae2c5d4c3993c4fa436
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2015-10-15 01:13:36 +0800

    doc + text node crom object

--- js/jsi.js
+++ js/jsi.js
@@ -80,7 +80,7 @@ Jsi.doceleqFirst = function (query) {
 * @return {DOMElement} object
 */
 Jsi.append = function (parent, elt) {
-	if (elt.tag !== undefined) {
+	if (elt.tag !== undefined || elt.isTextNode !== undefined ) {
 		elt = this.elt(elt);
 	}
 	if (typeof parent != 'object') {
@@ -96,8 +96,8 @@ Jsi.append = function (parent, elt) {
 
 /**
 * append an element before another
-* @param {mixed} elt the element to append to or its id
-* @param {mixed} n_elt DOMElement or template object to append to parent
+* @param elt mixed the element to append to or its id
+* @param n_elt mixed DOMElement or template object to append to parent
 * @return object DOMElement
 */
 Jsi.appendBefore = function (elt, n_elt) {
@@ -117,8 +117,8 @@ Jsi.appendBefore = function (elt, n_elt) {
 
 /**
 * append an element after another
-* @param {mixed} elt the element to append to or its id
-* @param {mixed} n_elt DOMElement or template object to append to parent
+* @param elt mixed the element to append to or its id
+* @param n_elt mixed DOMElement or template object to append to parent
 * @return object DOMElement
 */
 Jsi.appendAfter = function (elt, n_elt) {
@@ -138,8 +138,8 @@ Jsi.appendAfter = function (elt, n_elt) {
 
 /**
 * append an element to a parent as its first child
-* @param {mixed} parent the element to append to or its id
-* @param {mixed} elt DOMElement or template object to append to parent
+* @param parent mixed the element to append to or its id
+* @param elt mixed DOMElement or template object to append to parent
 * @return object DOMElement
 */
 Jsi.prepend = function (parent, elt) {
@@ -162,9 +162,7 @@ Jsi.prepend = function (parent, elt) {
 
 /**
 * replace an element
-* @param {DOMElement} elt the element to be replaced
-* @param {DOMElement} n_elt the new element
-* @return {DOMElement} the parent node of the element
+* @return object DOMElement
 */
 Jsi.replace = function (elt, n_elt) {
 	if (n_elt.tag !== undefined) {
@@ -184,8 +182,7 @@ Jsi.replace = function (elt, n_elt) {
 
 /**
 * create a text node
-* @param {string} content the content of the text node
-* @return {DOMText}
+* @param content string the content of the text node
 */
 Jsi.textNode = function(content){
 	return document.createTextNode(content);
@@ -193,8 +190,8 @@ Jsi.textNode = function(content){
 
 /**
 * create an element from a template object
-* @param {object} c the template object to be created
-* @return {DOMElement} object
+* @param c object the template object to be created
+* @return object DOMElement
 *<exemple>
 *{
 *	"tag":"type of element",
@@ -208,158 +205,159 @@ Jsi.textNode = function(content){
 */
 Jsi.elt = function(c) {
 	var elt = null;
-	// check if the element tag has already been created to clone it
-	if (this.created[c.tag] === undefined) {
-		this.created[c.tag] = document.createElement(c.tag);
-		elt = this.created[c.tag].cloneNode();
+	if (c.isTextNode !== undefined) {
+		elt = this.textNode(c.content);
 	}
-	else {
-		elt = this.created[c.tag].cloneNode();		
-	}
-	// create element attribute
-	if(c.attr !== undefined)
-	{
-		for(var key in c.attr)
+	else{
+		// check if the element tag has already been created to clone it
+		if (this.created[c.tag] === undefined) {
+			this.created[c.tag] = document.createElement(c.tag);
+			elt = this.created[c.tag].cloneNode();
+		}
+		else {
+			elt = this.created[c.tag].cloneNode();		
+		}
+		// create element attribute
+		if(c.attr !== undefined)
 		{
-			// handle element event or certain specific attribute
-			switch(key){
-				case 'onclick':
-					elt.addEventListener('click', c.attr[key]);
-					break;
-				case 'onchange':
-					elt.addEventListener('change', c.attr[key]);
-					break;
-				case 'onfocus':
-					elt.addEventListener('focus', c.attr[key]);
-					break;
-				case 'onkeyup':
-					elt.addEventListener('keyup', c.attr[key]);
-					break;
-				case 'onkeypress':
-					elt.addEventListener('keypress', c.attr[key]);
-					break;
-				case 'onsubmit':
-					elt.addEventListener('submit', c.attr[key]);
-					break;
-				case 'onanimationend':
-					elt.addEventListener(Jsi.animationend, c.attr[key]);
-					break;
-				case 'ontransitionend':
-					elt.addEventListener(Jsi.transitionend, c.attr[key]);
-					break;
-				case 'oncopy':
-					elt.addEventListener('copy', c.attr[key]);
-					break;
-				case 'oncut':
-					elt.addEventListener('cut', c.attr[key]);
-					break;
-				case 'ondblclick':
-					elt.addEventListener('dblclick', c.attr[key]);
-					break;
-				case 'oninvalid':
-					elt.addEventListener('invalid', c.attr[key]);
-					break;
-				case 'onmousedown':
-					elt.addEventListener('mousedown', c.attr[key]);
-					break;
-				case 'onmouseup':
-					elt.addEventListener('mouseup', c.attr[key]);
-					break;
-				case 'onmouseenter':
-					elt.addEventListener('mouseenter', c.attr[key]);
-					break;
-				case 'onmouseleave':
-					elt.addEventListener('mouseleave', c.attr[key]);
-					break;
-				case 'onmousemove':
-					elt.addEventListener('mousemove', c.attr[key]);
-					break;
-				case 'onmouseout':
-					elt.addEventListener('mouseout', c.attr[key]);
-					break;
-				case 'onmouseover':
-					elt.addEventListener('mouseover', c.attr[key]);
-					break;
-				case 'onpaste':
-					elt.addEventListener('paste', c.attr[key]);
-					break;
-				case 'onresize':
-					elt.addEventListener('resize', c.attr[key]);
-					break;
-				case 'onscroll':
-					elt.addEventListener('scroll', c.attr[key]);
-					break;
-				case 'onselect':
-					elt.addEventListener('select', c.attr[key]);
-					break;
-				case 'ontouchstart':
-					elt.addEventListener('touchstart', c.attr[key]);
-					break;
-				case 'ontouchmove':
-					elt.addEventListener('touchmove', c.attr[key]);
-					break;
-				case 'ontouchend':
-					elt.addEventListener('touchend', c.attr[key]);
-					break;
-				case 'checked':
-					elt.checked = c.attr[key];
-					break;
-				case 'className':
-					elt.setAttribute('class',c.attr[key]);
-					break;
-				default:
-					elt.setAttribute(key,c.attr[key]);
-					break;
+			for(var key in c.attr)
+			{
+				// handle element event or certain specific attribute
+				switch(key){
+					case 'onclick':
+						elt.addEventListener('click', c.attr[key]);
+						break;
+					case 'onchange':
+						elt.addEventListener('change', c.attr[key]);
+						break;
+					case 'onfocus':
+						elt.addEventListener('focus', c.attr[key]);
+						break;
+					case 'onkeyup':
+						elt.addEventListener('keyup', c.attr[key]);
+						break;
+					case 'onkeypress':
+						elt.addEventListener('keypress', c.attr[key]);
+						break;
+					case 'onsubmit':
+						elt.addEventListener('submit', c.attr[key]);
+						break;
+					case 'onanimationend':
+						elt.addEventListener(Jsi.animationend, c.attr[key]);
+						break;
+					case 'ontransitionend':
+						elt.addEventListener(Jsi.transitionend, c.attr[key]);
+						break;
+					case 'oncopy':
+						elt.addEventListener('copy', c.attr[key]);
+						break;
+					case 'oncut':
+						elt.addEventListener('cut', c.attr[key]);
+						break;
+					case 'ondblclick':
+						elt.addEventListener('dblclick', c.attr[key]);
+						break;
+					case 'oninvalid':
+						elt.addEventListener('invalid', c.attr[key]);
+						break;
+					case 'onmousedown':
+						elt.addEventListener('mousedown', c.attr[key]);
+						break;
+					case 'onmouseup':
+						elt.addEventListener('mouseup', c.attr[key]);
+						break;
+					case 'onmouseenter':
+						elt.addEventListener('mouseenter', c.attr[key]);
+						break;
+					case 'onmouseleave':
+						elt.addEventListener('mouseleave', c.attr[key]);
+						break;
+					case 'onmousemove':
+						elt.addEventListener('mousemove', c.attr[key]);
+						break;
+					case 'onmouseout':
+						elt.addEventListener('mouseout', c.attr[key]);
+						break;
+					case 'onmouseover':
+						elt.addEventListener('mouseover', c.attr[key]);
+						break;
+					case 'onpaste':
+						elt.addEventListener('paste', c.attr[key]);
+						break;
+					case 'onresize':
+						elt.addEventListener('resize', c.attr[key]);
+						break;
+					case 'onscroll':
+						elt.addEventListener('scroll', c.attr[key]);
+						break;
+					case 'onselect':
+						elt.addEventListener('select', c.attr[key]);
+						break;
+					case 'ontouchstart':
+						elt.addEventListener('touchstart', c.attr[key]);
+						break;
+					case 'ontouchmove':
+						elt.addEventListener('touchmove', c.attr[key]);
+						break;
+					case 'ontouchend':
+						elt.addEventListener('touchend', c.attr[key]);
+						break;
+					case 'checked':
+						elt.checked = c.attr[key];
+						break;
+					case 'className':
+						elt.setAttribute('class',c.attr[key]);
+						break;
+					default:
+						elt.setAttribute(key,c.attr[key]);
+						break;
+				}
 			}
 		}
-	}
 
-	// in some case you want to be able to force innerHTML value, 
-	// /!\ THIS IS BAAAAAAD !
-	if (c.innerForce !== undefined) {
-		elt.innerHTML = c.innerForce;
-	}
+		// in some case you want to be able to force innerHTML value, 
+		// /!\ THIS IS BAAAAAAD !
+		if (c.innerForce !== undefined) {
+			elt.innerHTML = c.innerForce;
+		}
 
-	// append elements if there are some 
-	if (c.append !== undefined) {
-		for (var ap in c.append) {
-			if (c.append[ap].tag !== undefined) {
-				elt = this.append(elt, this.elt(c.append[ap]));
-			}
-			else{
-				elt = this.append(elt, c.append[ap]);
+		// append elements if there are some 
+		if (c.append !== undefined) {
+			for (var ap in c.append) {
+				if (c.append[ap].tag !== undefined) {
+					elt = this.append(elt, this.elt(c.append[ap]));
+				}
+				else{
+					elt = this.append(elt, c.append[ap]);
+				}
 			}
 		}
-	}
 
-	// prepend elements
-	if (c.prepend !== undefined) {
-		for (var pp in c.prepend) {
-			if (c.prepend[pp].tag !== undefined) {
-				elt = this.prepend(elt, this.elt(c.prepend[pp]));
-			}
-			else{
-				elt = this.prepend(c.prepend[pp]);
+		// prepend elements
+		if (c.prepend !== undefined) {
+			for (var pp in c.prepend) {
+				if (c.prepend[pp].tag !== undefined) {
+					elt = this.prepend(elt, this.elt(c.prepend[pp]));
+				}
+				else{
+					elt = this.prepend(c.prepend[pp]);
+				}
 			}
 		}
-	}
-	
-	// create inner text content node and append it
-	if (c.inner !== undefined){
-		elt.appendChild(this.textNode(c.inner));
-	}
+		
+		// create inner text content node and append it
+		if (c.inner !== undefined){
+			elt.appendChild(this.textNode(c.inner));
+		}
 
-	if (c.initCallback !== undefined && typeof c.initCallback == 'function') {
-		c.initCallback(elt);
+		if (c.initCallback !== undefined && typeof c.initCallback == 'function') {
+			c.initCallback(elt);
+		}
 	}
 
 	return elt;
 };
-/**
- * create a DOMElement from a string
- * @param  {strin} elt the string to be converted
- * @return {DOMElement}
- */
+
 Jsi.eltFromString = function (elt) {
 	var el = elt;
 	try{
@@ -374,10 +372,9 @@ Jsi.eltFromString = function (elt) {
 	return elt;
 };
 
-/**
+/*
 * destroy a Dom element
-* @param {mixed} elt id of the element or element itself
-* @return {DOMElement} the removed child element
+* @param elt mixed id of the element or element itself
 */
 Jsi.remove = function (elt) {
 	if (typeof elt != 'object') {
@@ -393,7 +390,6 @@ Jsi.remove = function (elt) {
 
 /**
 * empty Dom element from it's children.
-* @param {DOMElement} elt the element to empty
 */
 Jsi.empty = function (elt) {
 	if (typeof elt != 'object') {
